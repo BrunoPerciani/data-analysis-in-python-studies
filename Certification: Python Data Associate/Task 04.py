d48e00701d1e1236ad35a3ccf379862a97f48519
Bruno Perciani <117026795+BrunoPerciani@users.noreply.github.com>
2026-03-16 15:32:20 -0300

    Update Task 04.py

--- Certification: Python Data Associate/Task 04.py
+++ Certification: Python Data Associate/Task 04.py
@@ -1,3 +1,14 @@
+# In order to proceed with further analysis later, you need to analyze how various factors relate to product quality. Start by calculating the mean and standard deviation for the following columns: `pigment_quantity`, and `product_quality_score`. </br> These statistics will help in understanding the central tendency and variability of the data related to product quality.
+# Next, calculate the Pearson correlation coefficient between the following variables: `pigment_quantity`, and `product_quality_score`.
+# These correlation coefficients will provide insights into the strength and direction of the relationships between the factors and overall product quality.
+
+# - You should start with the data in the file 'production_data.csv'.
+# - Calculate the mean and standard deviation for the columns pigment_quantity and product_quality_score as: `product_quality_score_mean`, `product_quality_score_sd`, `pigment_quantity_mean`, `pigment_quantity_sd`.
+# - Calculate the Pearson correlation coefficient between pigment_quantity and product_quality_score as: `corr_coef`
+# - Your output should be a DataFrame named product_quality.
+# - It should include the columns: `product_quality_score_mean`, `product_quality_score_sd`, `pigment_quantity_mean`, `pigment_quantity_sd`, `corr_coef`.
+# - Ensure that your answers are rounded to 2 decimal places.
+
 import pandas as pd
 
 df4 = pd.read_csv('production_data.csv')
